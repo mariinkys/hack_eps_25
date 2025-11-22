0562c6f3ad11b90a19206e9d246ab5dc009ec833
mariinkys <kysgeneral.nrgua@slmail.me>
2025-11-22 14:12:32 +0000

    improv: don't blink light, keep on until interrupt

--- main.py
+++ main.py
@@ -14,24 +14,24 @@ def connect_to_plc(ip="192.168.0.1", rack=0, slot=1):
 
     return client
 
-def blink_light(client, db_number, byte_index, bit_index, duration=1):
+def on_light(client, db_number, byte_index, bit_index):
     """
-    Enciende y apaga un bit en un DB concreto (parpadeo visible).
+    Enciende y apaga un bit en un DB concreto.
     """
 
-    # Leer 1 byte del DB donde está la luz
     data = client.read_area(Areas.DB, db_number, byte_index, 1)
 
     print(f"[+] Luz en DB{db_number}.BYTE{byte_index}.BIT{bit_index} → ON")
     set_bool(data, 0, bit_index, True)
     client.write_area(Areas.DB, db_number, byte_index, data)
 
-    time.sleep(duration)
-
-    print(f"[-] Luz en DB{db_number}.BYTE{byte_index}.BIT{bit_index} → OFF")
-    set_bool(data, 0, bit_index, False)
-    client.write_area(Areas.DB, db_number, byte_index, data)
-
-
 plc = connect_to_plc("10.72.101.68", rack=0, slot=1)
-blink_light(plc, db_number=1, byte_index=0, bit_index=0, duration=10)
+on_light(plc, db_number=1, byte_index=0, bit_index=0)
+on_light(plc, db_number=2, byte_index=0, bit_index=0)
+
+try:
+    while True:
+        time.sleep(1)  
+except KeyboardInterrupt:
+    print("Script detenido por el usuario.")
+    plc.disconnect()
